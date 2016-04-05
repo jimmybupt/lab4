da28af76e6b448fc0ed15d2a4d5b4e15cc21750c
Kun Liu <terryliu0301@gmail.com>
2016-04-05 14:53:33 -0400

    true sim

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 print 'CSE 5243 Similarity Analysis by Kun Liu & Zhe Dong'
 
 import load
+import minhash
+import time
+
 (Data, Cnt, rdim, cdim) = load.load()
 
 #convert data into 0/1
@@ -10,12 +13,24 @@ import load
 #			Data[i][j] = 1
 #print "done1"
 
-#compute true similarity
-True_Sim = []
 #import sklearn.metrics.pairwise
 #Pair_Wise_Dist = sklearn.metrics.pairwise.pairwise_distances(Data,metric='jaccard')
 #print "done2"
 
+#Mean Squared Error
+def similarity_MSE(true_sim, hash_sim):
+    mse = 0
+    for i in range(0,len(true_sim)):
+        for j in range(0, len(true_sim[i]) ):
+            mse += pow(true_sim[i][j] - hash_sim[i][j],2)
+    
+    return mse / len(true_sim)
+    
+    
+#compute true similarity
+start_time = time.time()
+print "Calculate true similarities"
+True_Sim = []
 for i in range(0, rdim):
 	print "document #"+str(i)
 	row_sim = []
@@ -25,8 +40,7 @@ for i in range(0, rdim):
 		sim = float(N[0,0])/float(U)
 		row_sim.append(sim)
 	True_Sim.append(row_sim)
-print "done"
-
-import minhash
+print ""
+print "True similarity calculation took  "+str(time.time() - start_time)+" seconds"
 
 M = minhash(cdim, 32)
